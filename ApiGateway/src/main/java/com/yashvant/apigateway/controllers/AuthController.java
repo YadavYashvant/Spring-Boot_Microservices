6d42be8631ba29a7465b276cdda825443b2ac49f
yadavyashvant <yashvantyadav855@gmail.com>
2024-01-03 08:33:49 +0530

    fixing AuthController

--- ApiGateway/src/main/java/com/yashvant/apigateway/controllers/AuthController.java
+++ ApiGateway/src/main/java/com/yashvant/apigateway/controllers/AuthController.java
@@ -1,4 +1,9 @@
 package com.yashvant.apigateway.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthController {
+
 }
